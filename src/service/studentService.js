6cf208f5a7d9f6032246bb464811cd23428e16d2
edward <edwardlfish@gmail.com>
2026-06-22 20:36:34 +0300

    Implement Student model, repository, and service with core functionalities

--- src/service/studentService.js
+++ src/service/studentService.js
@@ -1,31 +1,51 @@
-export const addStudent = async (student) => {
-    // TODO: Implement addStudent, Return true if student was added successfully and false otherwise
-}
+import * as repo from '../repository/studentRepository.js';
+
+export const addStudent = async (student) => repo.createStudent(student);
 
 export const findStudent = async (id) => {
-    // TODO: Implement findStudent
+    let student = repo.findStudentById(+id);
+    if (student) {
+        student = {...student};
+        student.password = undefined;
+    }
+    return student;
 }
 
 export const deleteStudent = async (id) => {
-    // TODO: Implement deleteStudent
+    let student = repo.deleteStudent(+id);
+    if (student) {
+        student.password = undefined;
+    }
+    return student;
 }
 
 export const updateStudent = async (id, data) => {
-    // TODO: Implement updateStudent
+    let student = repo.findStudentById(+id);
+    if (student) {
+        student = {...student, ...data};
+        student = {...repo.updateStudent(student)};
+        student.scores = undefined;
+    }
+    return student;
 }
 
 export const addScore = async (id, exam, score) => {
-    // TODO: Implement addScore
+    const student = repo.findStudentById(+id);
+    if (student) {
+        student.scores[exam] = score;
+        repo.updateStudent(student);
+    }
+    return student;
 }
 
-export const findStudentsByName = async (name) => {
-    // TODO: Implement findStudentsByName
-}
+export const findStudentsByName = async (name) => repo.findStudentsByName(name).map(s => ({...s, password: undefined}));
 
 export const countStudentsByNames = async (names) => {
-    // TODO: Implement countStudentsByNames
+    names = Array.isArray(names) ? names : [names];
+    return repo.countStudentsByNames(names);
 }
 
-export const findStudentsByMinScore = async (exam, minScore) => {
-    // TODO: Implement findStudentsByMinScore
-}
+export const findStudentsByMinScore = async (exam, minScore) => repo.findStudentsByMinScore(exam, minScore).map(s => ({
+    ...s,
+    password: undefined
+}));
